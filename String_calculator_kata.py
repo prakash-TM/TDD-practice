6f9b3bd90083d92c07fcf90bd5962e4d50a091d6
Prakash T M <59185088+prakash-TM@users.noreply.github.com>
2023-02-06 17:22:46 +0530

    Update String_calculator_kata.py

--- String_calculator_kata.py
+++ String_calculator_kata.py
@@ -1,20 +1,20 @@
-s = input()
-t=""
-a=[]
-tot = 0
-for i in range(len(s)):
-    if (s[i] == "-" and ("0" <= s[i+1] <= "9")):
+s = input()  #getting input
+t=""     #temporary variable
+a=[]    # empty array initialization.
+tot = 0 #total - storing result.
+for i in range(len(s)):    # getting single element from input string.
+    if (s[i] == "-" and ("0" <= s[i+1] <= "9")):  # check if the element is negative numbers.
         print ("negatives not allowed")
-    elif ("0" <= s[i] <= "9"):
-        t=t+s[i]
+    elif ("0" <= s[i] <= "9"):    # check if the element is numeric.
+        t=t+s[i]          #concadinate numeric values as string.
     else:
-        if (t == ""):
+        if (t == ""):    # check if the element is white space.
             a.append("0")
         else:
             a.append(t)
         t=""
-a.append(t)
+a.append(t)       # update the arry with end result.
 for j in a:
-    if (int (j) < 1000 and int(j) != 0):
-        tot += int(j)
-print(tot)
+    if (int (j) < 1000 and int(j) != 0):  # check if the stored array element is not 0 and less then 1000.
+        tot += int(j)      #convert the element to int and concadinate to the total variable.
+print(tot)   #Print end result
